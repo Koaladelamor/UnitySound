1c0b7d91320b3978f3c34b67df7ee9457e825ea2
Marc Soriano <marc.soriano6@gmail.com>
2022-05-15 23:24:01 +0200

    FireSound

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -17,33 +17,20 @@ public class SoundManager : MonoBehaviour
     public AudioSource effectsSounds;
     public AudioSource BackgroungMusic;
 
-    public AudioClip selectSound;
-    public AudioClip pressedSound;
-    public AudioClip bagCloseSound;
-    public AudioClip bagOpenSound;
-    public AudioClip arrowHitFlesh;
-    public AudioClip beOnYourGuard;
-    public AudioClip birdSound;
-    public AudioClip chestOpeningSound;
-    public AudioClip coinDroppedSound;
-    public AudioClip dramaticBirdsSound;
-    public AudioClip owlSound;
-    public AudioClip wolfHowlSound;
-    public AudioClip swordHitFleshSound;
-    public AudioClip swordHitObjectSound;
-    public AudioClip swordHitMetalSound;
-    public AudioClip GrodnarDeathSound;
-    public AudioClip SigfridDeathSound;
-    public AudioClip LanstarDeathSound;
+    public AudioClip rightFootstep;
+    public AudioClip leftFootstep;
+    public AudioClip grassFootsteps;
+    public AudioClip metalGate;
+    public AudioClip metalGate2;
+    public AudioClip crowSound;
+    public AudioClip fireSound;
+    public AudioClip blessingMusic;
 
-    public AudioClip BellSound;
-    public AudioClip enemyHurt;
-    public AudioClip playerHurt;
-    public AudioClip lifeSound;
+    public AudioClip creepyAmbience;
 
-    public enum AudioEffects { NONE, SELECT, PRESSED, LIFESOUND };
+    public enum AudioEffects { NONE, RIGHT_FOOTSTEP, LEFT_FOOTSTEP };
 
-    public enum InstantAudios { NONE, ENEMYHURT, PLAYERHURT, BAGCLOSE, BAGOPEN, ARROWFLESH, ONGUARD, BIRD, CHESTOPEN, COIN, DRAMATICBIRD, OWL, WOLFHOWL, SWORDFLESH, SWORDOBJECT, SWORDMETAL, GRODNARDEATH, SIGFRIDDEATH, LANSTARDEATH, BELLSOUND };
+    public enum InstantAudios { NONE, FOOTSTEP_RIGHT, FOOTSTEP_LEFT, FOOTSTEP_GRASS, GATE, GATE2, CROW, FIRE, BLESSING };
 
     private InstantAudios instantAudios;
 
@@ -73,14 +60,11 @@ public class SoundManager : MonoBehaviour
         {
             case AudioEffects.NONE:
                 break;
-            case AudioEffects.SELECT:
-                effectsSounds.clip = selectSound;
+            case AudioEffects.RIGHT_FOOTSTEP:
+                effectsSounds.clip = rightFootstep;
                 break;
-            case AudioEffects.PRESSED:
-                effectsSounds.clip = pressedSound;
-                break;
-            case AudioEffects.LIFESOUND:
-                effectsSounds.clip = bagCloseSound;
+            case AudioEffects.LEFT_FOOTSTEP:
+                effectsSounds.clip = leftFootstep;
                 break;
             default:
                 break;
@@ -98,66 +82,32 @@ public class SoundManager : MonoBehaviour
         {
             case InstantAudios.NONE:
                 break;
-            case InstantAudios.ENEMYHURT:
-                effectsSounds.PlayOneShot(enemyHurt);
+            case InstantAudios.FOOTSTEP_RIGHT:
+                effectsSounds.PlayOneShot(rightFootstep);
                 break;
-            case InstantAudios.PLAYERHURT:
-                effectsSounds.PlayOneShot(playerHurt);
+            case InstantAudios.FOOTSTEP_LEFT:
+                effectsSounds.PlayOneShot(leftFootstep);
                 break;
-            case InstantAudios.BAGCLOSE:
-                effectsSounds.PlayOneShot(bagCloseSound);
+            case InstantAudios.FOOTSTEP_GRASS:
+                effectsSounds.PlayOneShot(grassFootsteps);
                 break;
-            case InstantAudios.BAGOPEN:
-                effectsSounds.PlayOneShot(bagOpenSound);
+            case InstantAudios.GATE:
+                effectsSounds.PlayOneShot(metalGate);
                 break;
-            case InstantAudios.ARROWFLESH:
-                effectsSounds.PlayOneShot(arrowHitFlesh);
+            case InstantAudios.GATE2:
+                effectsSounds.PlayOneShot(metalGate2);
                 break;
-            case InstantAudios.ONGUARD:
-                effectsSounds.PlayOneShot(beOnYourGuard);
+            case InstantAudios.CROW:
+                effectsSounds.PlayOneShot(crowSound);
                 break;
-            case InstantAudios.BIRD:
-                effectsSounds.PlayOneShot(birdSound);
+            case InstantAudios.FIRE:
+                effectsSounds.PlayOneShot(fireSound);
                 break;
-            case InstantAudios.CHESTOPEN:
-                effectsSounds.PlayOneShot(chestOpeningSound);
-                break;
-            case InstantAudios.COIN:
-                effectsSounds.PlayOneShot(coinDroppedSound);
-                break;
-            case InstantAudios.DRAMATICBIRD:
-                effectsSounds.PlayOneShot(dramaticBirdsSound);
-                break;
-            case InstantAudios.OWL:
-                effectsSounds.PlayOneShot(owlSound);
-                break;
-            case InstantAudios.WOLFHOWL:
-                effectsSounds.PlayOneShot(wolfHowlSound);
-                break;
-            case InstantAudios.SWORDFLESH:
-                effectsSounds.PlayOneShot(swordHitFleshSound);
-                break;
-            case InstantAudios.SWORDOBJECT:
-                effectsSounds.PlayOneShot(swordHitObjectSound);
-                break;
-            case InstantAudios.SWORDMETAL:
-                effectsSounds.PlayOneShot(swordHitMetalSound);
-                break;
-            case InstantAudios.GRODNARDEATH:
-                effectsSounds.PlayOneShot(GrodnarDeathSound);
-                break;
-            case InstantAudios.SIGFRIDDEATH:
-                effectsSounds.PlayOneShot(SigfridDeathSound);
-                break;
-            case InstantAudios.LANSTARDEATH:
-                effectsSounds.PlayOneShot(LanstarDeathSound);
-                break;
-            case InstantAudios.BELLSOUND:
-                effectsSounds.PlayOneShot(BellSound);
+            case InstantAudios.BLESSING:
+                effectsSounds.PlayOneShot(blessingMusic);
                 break;
             default:
                 break;
         }
-
     }
 }
